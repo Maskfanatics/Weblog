6c43c3aa032112311abb11d1326f0282175d4de5
shujin <shujin.jiang@greatopensource.com>
2024-06-22 16:49:19 +0800

    skeleton init

---
+++ weblog-module-common/src/main/java/com/shumujin/weblog/common/aspect/ApiOperationLog.java
@@ -0,0 +1,18 @@
+package com.shumujin.weblog.common.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+@Documented
+public @interface ApiOperationLog {
+    /**
+     * API 功能描述
+     *
+     * @return
+     */
+    String description() default "";
+
+}
